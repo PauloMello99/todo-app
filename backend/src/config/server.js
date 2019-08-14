b6d905d955c5306ed7cac8bddf728958c0a7eb18
Paulo Mello <paulomello@Mac-mini-de-Paulo.local>
2019-08-14 16:42:06 -0300

    Add Project

---
+++ backend/src/config/server.js
@@ -0,0 +1,12 @@
+const bodyparser = require('body-parser');
+const express = require('express');
+const port = 3003;
+const server = express();
+const allowCors = require('./cors');
+server.use(bodyparser.urlencoded({extended:true}));
+server.use(bodyparser.json());
+server.use(allowCors);
+server.listen(port,function(){
+    console.log(`Backend running on port ${port}`);
+});
+module.exports = server
---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet ....</p>
+        <br/>
+        <br/>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet ....</p>
+        <br/>
+        <br/>
+        <h2>imprensa</h2>
+        <p>Lorem ipsum dolor sit amet ....</p>
+    </div>
+)
